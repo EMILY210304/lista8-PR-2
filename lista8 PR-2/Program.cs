c65970f27eb848d0647e539e8cfd2ed5a920acd4
CJ3016994 <CJ3016994@LAB01-PC32>
2023-05-31 11:23:49 -0300

    Adicionar arquivos de projeto.

---
+++ lista8 PR-2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista8_PR_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //{
+  //              bool sair = false;
+
+  //              while (!sair)
+  //              {
+  //                  Console.WriteLine("Escolha uma opção:");
+  //                  Console.WriteLine("1 - Opção 1");
+  //                  Console.WriteLine("2 - Opção 2");
+  //                  Console.WriteLine("3 - Opção 3");
+  //                  Console.WriteLine("4 - Sair");
+
+  //                  Console.Write("Digite o número da opção desejada: ");
+  //                  int opcao = Convert.ToInt32(Console.ReadLine());
+
+  //                  switch (opcao)
+  //                  {
+  //                      case 1:
+  //                          Console.WriteLine("Você escolheu a opção 1");
+  //                          // Faça algo relacionado à opção 1
+  //                          break;
+  //                      case 2:
+  //                          Console.WriteLine("Você escolheu a opção 2");
+  //                          // Faça algo relacionado à opção 2
+  //                          break;
+  //                      case 3:
+  //                          Console.WriteLine("Você escolheu a opção 3");
+  //                          // Faça algo relacionado à opção 3
+  //                          break;
+  //                      case 4:
+  //                          sair = true;
+  //                          Console.WriteLine("Saindo do programa...");
+  //                          break;
+  //                      default:
+  //                          Console.WriteLine("Opção inválida. Digite um número válido.");
+  //                          break;
+  //                  }
+
+  //                  Console.ReadKey();
+  //              }
+  //          }
+  //      }
+
+  //  }
+  //}
+
